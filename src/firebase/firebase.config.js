e469ca09ac7421acfe07b25002cd9609b0a7b5dd
shahinur69 <shahinurmubarak238@gmail.com>
2026-08-24 12:43:12 +0600

    fix: update Firebase config to use VITE_ environment variable prefixes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3thk_a787NfsvOuspQKet9RtJvlI2_dk",
-  authDomain: "campusconnect-dbdd6.firebaseapp.com",
-  projectId: "campusconnect-dbdd6",
-  storageBucket: "campusconnect-dbdd6.firebasestorage.app",
-  messagingSenderId: "117513795694",
-  appId: "1:117513795694:web:7cab613b275cff87eb0e7f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
